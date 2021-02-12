2dbdbf2b0199de7f530349749fb7194ec4805a5e
mfazin <78682897+mfazin@users.noreply.github.com>
2021-02-12 18:35:24 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -12,7 +12,7 @@ async def cb_handler(c, m):
 
   if "feed" in cb_data:
       Config.feedback.append(m.from_user.id)
-      button = [[InlineKeyboardButton("cancel", callback_data="cancel")]]
+      button = [[InlineKeyboardButton("❎cancel❎", callback_data="cancel")]]
       markup = InlineKeyboardMarkup(button)
       await m.message.delete()
       await c.send_message(chat_id=m.message.chat.id, text="Send your feed back here I will notify the admin.", reply_markup=markup)
@@ -37,19 +37,19 @@ async def cb_handler(c, m):
   if "yes" in cb_data:
       Config.feedback.remove(m.from_user.id)
       feedtext = m.message.reply_to_message
-      button = [[InlineKeyboardButton("Reply", callback_data=f"reply+{m.from_user.id}")]]
+      button = [[InlineKeyboardButton("🔱Reply🔱", callback_data=f"reply+{m.from_user.id}")]]
       markup = InlineKeyboardMarkup(button)
       for i in Config.OWNER:
           NS = await feedtext.forward(int(i))
           await NS.reply_text("Send the reply", reply_markup=markup, quote=True)
       await m.message.delete()
-      await c.send_message(chat_id=m.message.chat.id, text="Feedback sent successfully. Hope you will get reply soon")
+      await c.send_message(chat_id=m.message.chat.id, text="Feedback sent successfully 😎. Hope you will get reply soon")
 
 
   if "reply" in cb_data:
       id = m.data.split("+")[1]
       Config.SEND.append(id)
-      await c.send_message(chat_id=m.message.chat.id, text="Reply me the text which you wantedto send him")
+      await c.send_message(chat_id=m.message.chat.id, text="Reply me the text which you wanted to send him")
 
   if "about" in cb_data:
       await m.message.delete()
